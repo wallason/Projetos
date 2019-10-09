de0607ec1e2fd72dfdc58626067b0eda1e774b5c
wallason <56327260+wallason@users.noreply.github.com>
2019-10-08 22:06:44 -0300

    Add files via upload

---
+++ Conversão/UnitConversão.pas
@@ -0,0 +1,66 @@
+unit UnitConversão;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Celsius: TLabel;
+    Fahrenheit: TLabel;
+    Edt1: TEdit;
+    Edt2: TEdit;
+    f: TButton;
+    C: TButton;
+    Conversão1: TPanel;
+    Conversão2: TPanel;
+    BtnLimpar: TButton;
+    procedure CClick(Sender: TObject);
+    procedure fClick(Sender: TObject);
+    procedure BtnLimparClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.BtnLimparClick(Sender: TObject);
+begin
+Edt1.Clear;
+Edt2.clear;
+Conversão1.Caption:='';
+Conversão2.Caption:='';
+
+end;
+
+procedure TForm1.CClick(Sender: TObject);
+var
+C,F : real;
+begin
+F:= strtofloat ( Edt2.Text);
+C:= 5/9 * (f-32);
+Conversão1.Caption := floattostr (C);
+
+end;
+
+procedure TForm1.fClick(Sender: TObject);
+var
+C,F : real;
+begin
+C:= strtofloat ( Edt1.Text);
+F:= (9*C+160)/5;
+Conversão2.Caption := floattostr (F);
+
+
+end;
+
+end.
